3b4daa8fbc5d513b712a78193867a4fddfc26cdb
paufrxn19 <164872421+paufrxn19@users.noreply.github.com>
2024-04-01 15:54:34 +0800

    Add files via upload

---
+++ Assignment2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int Num = 1; Num <= 100; Num++)
+            {
+                if (Num % 15 == 0)
+                {
+                    Console.WriteLine("Hello GoodBye");
+                }
+                else if (Num % 5 == 0)
+                {
+                    Console.WriteLine("GoodBye");
+                }
+                else if (Num % 3 == 0)
+                {
+                    Console.WriteLine("Hello");
+                }
+                else
+                {
+                    Console.WriteLine(Num);
+                }
+            }
+        }
+    }
+}
